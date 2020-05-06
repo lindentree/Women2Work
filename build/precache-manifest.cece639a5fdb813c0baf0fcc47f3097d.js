4da4c682c5cfbbef799cabcbe720de3b99ad3c5b
Jordis Man <jordisman@gmail.com>
2020-05-06 14:24:10 -0700

    Women2Work deployed

---
+++ build/precache-manifest.cece639a5fdb813c0baf0fcc47f3097d.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfdd1a0cb98458574795bc1f26142184",
+    "url": "/Women2Work/index.html"
+  },
+  {
+    "revision": "baf085bcb40981e7c2d2",
+    "url": "/Women2Work/static/css/main.2d1ceeee.chunk.css"
+  },
+  {
+    "revision": "638fa71f422d2dddc2bc",
+    "url": "/Women2Work/static/js/2.b3f2df09.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Women2Work/static/js/2.b3f2df09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baf085bcb40981e7c2d2",
+    "url": "/Women2Work/static/js/main.73657c93.chunk.js"
+  },
+  {
+    "revision": "45877cab94f101754dad",
+    "url": "/Women2Work/static/js/runtime-main.ad47511d.js"
+  },
+  {
+    "revision": "ee64b6d50406ec6100e1f61b45f0d428",
+    "url": "/Women2Work/static/media/Logo.ee64b6d5.png"
+  },
+  {
+    "revision": "8cfcad2e7f4a0bb3f212d2c2905f3d8f",
+    "url": "/Women2Work/static/media/badge.8cfcad2e.png"
+  },
+  {
+    "revision": "6789e7818951b0a218c05d8b6c6dfbfa",
+    "url": "/Women2Work/static/media/dots.6789e781.png"
+  },
+  {
+    "revision": "6afdc62e974d629abbb34a7134b90923",
+    "url": "/Women2Work/static/media/event1.6afdc62e.png"
+  },
+  {
+    "revision": "df877667e09f8d7cdd874635fc5c4a7f",
+    "url": "/Women2Work/static/media/event2.df877667.jpg"
+  },
+  {
+    "revision": "339e76a5a8b5de55d75a4de2d13ed815",
+    "url": "/Women2Work/static/media/event3.339e76a5.png"
+  },
+  {
+    "revision": "49dbff2be33a261de23cc4c22ae7aa92",
+    "url": "/Women2Work/static/media/faces.49dbff2b.png"
+  },
+  {
+    "revision": "6182e7b31d1574dc2a109ee38f9c844d",
+    "url": "/Women2Work/static/media/match.6182e7b3.png"
+  },
+  {
+    "revision": "0a0e918fb9de87d1ce227c3fda725ff0",
+    "url": "/Women2Work/static/media/pic1.0a0e918f.png"
+  },
+  {
+    "revision": "3ced9ab16d1cdd78493dea72ca948c12",
+    "url": "/Women2Work/static/media/pic2.3ced9ab1.png"
+  },
+  {
+    "revision": "8af6760a3879ee973c3e517c5b153435",
+    "url": "/Women2Work/static/media/pic3.8af6760a.png"
+  },
+  {
+    "revision": "6085b4d393212a6ccf56098acc4601f7",
+    "url": "/Women2Work/static/media/pic4.6085b4d3.png"
+  },
+  {
+    "revision": "e8454e5c8096ad666005c71027d02e12",
+    "url": "/Women2Work/static/media/pic5.e8454e5c.png"
+  },
+  {
+    "revision": "b07c9c8695bf23fa5c9750a9bcf9d620",
+    "url": "/Women2Work/static/media/pic6.b07c9c86.png"
+  },
+  {
+    "revision": "2c26ce27e13a2ab11b63f919f32b118e",
+    "url": "/Women2Work/static/media/pic7.2c26ce27.png"
+  },
+  {
+    "revision": "ba80864f56ff0d09d150b18280a1e853",
+    "url": "/Women2Work/static/media/pic8.ba80864f.png"
+  },
+  {
+    "revision": "0c8237a4c736596c7e24df69f0354993",
+    "url": "/Women2Work/static/media/pinkCircle.0c8237a4.png"
+  },
+  {
+    "revision": "0f0462f377e6f3bc4aa33fea91284d4b",
+    "url": "/Women2Work/static/media/smiley.0f0462f3.png"
+  },
+  {
+    "revision": "82d3895d931b849172b51b9639445c4a",
+    "url": "/Women2Work/static/media/star.82d3895d.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4b6854f3824b724b191a07af628b2383.js"
+  "/Women2Work/precache-manifest.cece639a5fdb813c0baf0fcc47f3097d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Women2Work/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
